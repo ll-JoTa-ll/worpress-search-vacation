0d99b9c4706da2358517275d223c6c1111d1eab9
JoTa <juan.caro.1987@gmail.com>
2021-10-12 18:02:05 -0500

    F

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   baseUrl: 'https://domiruth-prd.azure-api.net/vacation/',
   subsKey: '78e06a82cb2e4ddfb445f10d4ac6b36e',
   urlFlight: 'https://domiruth-prd.azure-api.net/flight/',
-  //urlVacaFacade: 'http://vacationprdsa.z13.web.core.windows.net/#/facade/',
-  urlVacaFacade: 'https://vacation.domiruth.com/#/facade/',
+  urlVacaFacade: 'http://vacationprdsa.z13.web.core.windows.net/#/facade/',
+  //urlVacaFacade: 'https://vacation.domiruth.com/#/facade/',
 };
